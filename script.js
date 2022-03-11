981b4bc23fee1494a50c9e977df5af7dbd5a8b98
Thanh Tung <thanhtung28400@gmail.com>
2022-03-11 12:16:56 +0700

    Make the clock functional

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ const hourHand = document.querySelector(".hand.hour");
 const minuteHand = document.querySelector(".hand.minute");
 const secondHand = document.querySelector(".hand.second");
 
-function setClock() {
-    let currentDate = new Date();
-    
+setInterval(() => {
+    let d = new Date();
+    let secondsRatio = d.getSeconds() / 60;
+    let minutesRatio = (secondsRatio + d.getMinutes()) / 60;
+    let hoursRatio = (minutesRatio + d.getHours()) / 12;
+
+    setHandRotation(secondHand, secondsRatio);
+    setHandRotation(minuteHand, minutesRatio);
+    setHandRotation(hourHand, hoursRatio);
+
+    // hourHand.style.transform = `translateX(-50%) rotate(${hoursRatio * 360}deg)`;
+    // minuteHand.style.transform = `translateX(-50%) rotate(${minutesRatio * 360}deg)`;
+    // secondHand.style.transform = `translate(-50%, 30px) rotate(${secondsRatio * 360}deg)`;
+}, 1000);
+
+function setHandRotation(element, rotationRatio) {
+    element.style.setProperty("--rotation", rotationRatio * 360);
 }
